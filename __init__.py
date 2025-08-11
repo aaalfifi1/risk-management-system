3f3187deb638b72d0a6eda4fc467e22841a0f4ba
aaalfifi1 <twag1212@gmail.com>
2025-08-12 02:16:28 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# الكود الصحيح
-from flask import Blueprint
-
-main_bp = Blueprint('main', __name__)
-
-# هذا السطر يستدعي ملف routes.py الموجود بجانبه
-# والذي يحتوي على كل المسارات مثل @main_bp.route('/')
-from . import routes 
